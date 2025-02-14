9458ac58d24f09b7e5f52081c2f03952cf0374f4
ShayMa3 <shayma@berkeley.edu>
2025-02-13 22:18:25 -0800

    Updates

---
+++ precache-manifest.b3b8ba0029c89fd63c009623a168f926.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e46944575557183b4be7e67bc7f60e4",
+    "url": "/fe-assessment/index.html"
+  },
+  {
+    "revision": "4fa50740cc5432d7565e",
+    "url": "/fe-assessment/static/css/main.78ac535f.chunk.css"
+  },
+  {
+    "revision": "94268418ba22974836b9",
+    "url": "/fe-assessment/static/js/2.ebc7ba11.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fe-assessment/static/js/2.ebc7ba11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fa50740cc5432d7565e",
+    "url": "/fe-assessment/static/js/main.a4dd3ecf.chunk.js"
+  },
+  {
+    "revision": "6cc3d4e3dac6ce9febcd",
+    "url": "/fe-assessment/static/js/runtime-main.a435b52f.js"
+  }
+]);
